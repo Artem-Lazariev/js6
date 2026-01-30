959337ce8c02573743b0efbc1a7f7fda0ca0100f
Lazarev Artem <lazarievartem25@gmail.com>
2026-01-30 17:18:12 +0200

    artem

---
+++ script.js
@@ -0,0 +1,113 @@
+let pick = "tea"
+let str = "your pick is "
+switch (pick){
+    case "tea":
+        console.log(str + pick)
+        break;
+    case "coffee":
+        console.log(str + pick)
+        break;
+    case "juce":
+        console.log(str + pick)
+        break;
+    default :
+        console.log("sorry, we don't have that")
+}
+let day = "monday"
+
+switch (day){
+    case "monday":
+        console.log("робочий день ")
+        break;
+    case "tuesday":
+        console.log("робочий день ")
+        break;
+    case "wednesday":
+        console.log("робочий день ")
+        break;
+    case "thursday":
+        console.log("робочий день ")
+        break;
+    case "friday":
+        console.log("робочий день ")
+        break;
+    case "saturday":
+        console.log("вихідний день ")
+        break;
+    case "sunday":
+        console.log("вихідний день ")
+        break;
+}
+let month = 4
+
+switch (month) {
+    case 1:
+        console.log("Зима");
+        break;
+    case 2:
+        console.log("Зима");
+        break;
+    case 3:
+        console.log("Весна");
+        break;
+    case 4:
+        console.log("Весна");
+        break;
+    case 5:
+        console.log("Весна");
+        break;
+    case 6:
+        console.log("Літо");
+        break;
+    case 7:
+        console.log("Літо");
+        break;
+    case 8:
+        console.log("Літо");
+        break;
+    case 9:
+        console.log("Осінь");
+        break;
+    case 10:
+        console.log("Осінь");
+        break;
+    case 11:
+        console.log("Осінь");
+        break;
+    case 12:
+        console.log("Зима");
+        break;
+}
+let color = "зелений";
+
+switch (color) {
+    case "червоний":
+        console.log("стоп");
+        break;
+    case "зелений":
+        console.log("йти");
+        break;
+    case "жовтий":
+        console.log("чекати");
+        break;
+}
+let num1 = 10
+let num2 = 5
+let operator = "/"
+
+switch (operator) {
+    case "+":
+        console.log(num1 + num2);
+        break;
+    case "-":
+        console.log(num1 - num2);
+        break;
+    case "*":
+        console.log(num1 * num2);
+        break;
+    case "/":
+
+        console.log(num1 / num2);
+
+        break;
+}
